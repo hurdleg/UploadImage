15c90861525a4d0234a3f288ff5df25359d6199e
Gerald Hurdle <hurdleg@algonquincollege.com>
2018-01-14 00:42:11 -0500

    Upgraded AS to version 3.x

--- app/src/main/java/mad9132/planets/MainActivity.java
+++ app/src/main/java/mad9132/planets/MainActivity.java
@@ -192,8 +192,8 @@ public class MainActivity extends Activity {
         requestPackage.setParam("overview", pluto.getOverview() );
         requestPackage.setParam("image", pluto.getImage() );
         requestPackage.setParam("description", pluto.getDescription() );
-        requestPackage.setParam("distance_from_sun", pluto.getDistanceFromSun() + "");
-        requestPackage.setParam("number_of_moons", pluto.getNumberOfMoons() + "" );
+        requestPackage.setParam("distanceFromSun", pluto.getDistanceFromSun() + "");
+        requestPackage.setParam("numberOfMoons", pluto.getNumberOfMoons() + "" );
 
         Intent intent = new Intent(this, MyService.class);
         intent.putExtra(MyService.REQUEST_PACKAGE, requestPackage);
@@ -284,8 +284,8 @@ public class MainActivity extends Activity {
         requestPackage.setParam("overview", pluto.getOverview() );
         requestPackage.setParam("image", pluto.getImage() );
         requestPackage.setParam("description", pluto.getDescription() );
-        requestPackage.setParam("distance_from_sun", pluto.getDistanceFromSun() + "");
-        requestPackage.setParam("number_of_moons", pluto.getNumberOfMoons() + "" );
+        requestPackage.setParam("distanceFromSun", pluto.getDistanceFromSun() + "");
+        requestPackage.setParam("numberOfMoons", pluto.getNumberOfMoons() + "" );
 
         Intent intent = new Intent(this, MyService.class);
         intent.putExtra(MyService.REQUEST_PACKAGE, requestPackage);
